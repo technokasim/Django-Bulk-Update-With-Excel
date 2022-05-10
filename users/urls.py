ed0ec3303c6a34f2b4ce04f9ebf9a123fb7633fe
technokasim <technokasim@gmail.com>
2022-05-10 21:21:16 +0400

    first commit

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path , include
+from .views import *
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('login', login_request, name='login'),
+    path('add-data', addData, name='addData'),
+    path('logout', logout_request, name='logout'),
+
+    path('ajax_add', ajax_add, name='ajax_add'),
+    path('ajax_update', ajax_update, name='ajax_update'),
+    path('ajax_delete', ajax_delete, name='ajax_delete'),
+
+    path('create/', CreateExcelView.as_view(), name='create'),
+    path('update/', UpdateExcelView.as_view(), name='update'),
+]
